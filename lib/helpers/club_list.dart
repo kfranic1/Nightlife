2875da33ef3d0dd4769726cdd7b7d26b0bb04ac2
kfranic1 <karlofranic0@gmail.com>
2023-08-04 01:26:26 +0200

    Add `typeOfMusic` filter and rearrange filters

--- lib/helpers/club_list.dart
+++ lib/helpers/club_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:nightlife/firestore/firestore_service.dart';
 import 'package:nightlife/helpers/filters.dart';
 
+import '../enums/type_of_music.dart';
 import '../model/club.dart';
 
 class ClubList extends ChangeNotifier {
@@ -9,6 +10,7 @@ class ClubList extends ChangeNotifier {
   List<Club> _filteredClubs = [];
 
   String _filterText = "";
+  TypeOfMusic? _typeOfMusic;
   BaseFilter _filter = BaseFilter.filters.first;
 
   Future setup() async {
@@ -20,6 +22,7 @@ class ClubList extends ChangeNotifier {
   }
 
   String get filterText => _filterText;
+  TypeOfMusic? get typeOfMusic => _typeOfMusic;
   BaseFilter get filter => _filter;
   List<Club> get clubs => _clubs;
   List<Club> get filteredClubs => _filteredClubs;
@@ -34,8 +37,14 @@ class ClubList extends ChangeNotifier {
     _applyFilter();
   }
 
+  void updateTypeOfMusic(TypeOfMusic? typeOfMusic) {
+    _typeOfMusic = typeOfMusic;
+    _applyFilter();
+  }
+
   void _applyFilter() {
     _filteredClubs = _clubs.where((club) => club.name.toLowerCase().contains(_filterText.toLowerCase())).toList();
+    if (_typeOfMusic != null) _filteredClubs = _filteredClubs.where((element) => element.typeOfMusic.contains(_typeOfMusic)).toList();
     _filter.applyFilter(_filteredClubs);
     notifyListeners();
   }
--- lib/widgets/filter.dart
+++ lib/widgets/filter.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nightlife/enums/type_of_music.dart';
+import 'package:nightlife/extensions/list_extension.dart';
 import 'package:nightlife/helpers/club_list.dart';
 import 'package:nightlife/helpers/default_box_decoration.dart';
 import 'package:nightlife/helpers/filters.dart';
@@ -24,49 +26,93 @@ class _FilterState extends State<Filter> {
   @override
   Widget build(BuildContext context) {
     ClubList clubs = context.watch<ClubList>();
-    return Row(
+    return Column(
       children: [
-        Expanded(
-          child: SizedBox(
-            height: 48,
-            child: TextField(
-              controller: _searchController,
-              onChanged: (value) => clubs.updateText(value),
-              decoration: InputDecoration(
-                labelText: 'Search',
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0)),
-              ),
+        SizedBox(
+          height: 48,
+          child: TextField(
+            controller: _searchController,
+            onChanged: (value) => clubs.updateText(value),
+            decoration: InputDecoration(
+              labelText: 'Search',
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0)),
             ),
           ),
         ),
-        const SizedBox(width: 12),
-        Container(
-          height: 48,
-          width: 140,
-          padding: const EdgeInsets.symmetric(horizontal: 12.0),
-          decoration: DefaultBoxDecoration(),
-          child: DropdownButton<BaseFilter>(
-            value: clubs.filter,
-            isExpanded: true,
-            icon: const Icon(Icons.arrow_drop_down),
-            iconSize: 24,
-            elevation: 16,
-            style: const TextStyle(color: Colors.black),
-            underline: Container(),
-            focusColor: Colors.transparent,
-            onChanged: (BaseFilter? selectedFilter) => clubs.updateFilter(selectedFilter!),
-            items: BaseFilter.filters
-                .map((baseFilter) => DropdownMenuItem<BaseFilter>(
-                      value: baseFilter,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: Text(baseFilter.name, style: const TextStyle(fontSize: 16, color: Colors.black)),
-                      ),
-                    ))
-                .toList(),
-          ),
+        const SizedBox(height: 8),
+        Row(
+          children: [
+            DropdownFilter<TypeOfMusic?>(
+              value: clubs.typeOfMusic,
+              onChanged: (TypeOfMusic? type) => clubs.updateTypeOfMusic(type),
+              label: "Genre",
+              items: Map.fromIterable(
+                TypeOfMusic.values.toList().rearrange((p0, p1) => p0.name.compareTo(p1.name)),
+                key: (element) => element,
+                value: (element) => element == null ? "None" : element.toString(),
+              )..addAll({null: "None"}),
+            ),
+            const SizedBox(width: 8),
+            DropdownFilter<BaseFilter>(
+              label: "Sort",
+              value: clubs.filter,
+              onChanged: (BaseFilter? selectedFilter) => clubs.updateFilter(selectedFilter!),
+              items: Map.fromIterable(BaseFilter.filters, key: (element) => element, value: (element) => element.name),
+            ),
+          ],
         ),
       ],
+    );
+  }
+}
+
+class DropdownFilter<T> extends StatelessWidget {
+  const DropdownFilter({
+    super.key,
+    required this.label,
+    required this.onChanged,
+    required this.items,
+    required this.value,
+  });
+
+  final String label;
+  final void Function(T?)? onChanged;
+  final Map<T, String> items;
+  final T value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.only(left: 12.0),
+        decoration: DefaultBoxDecoration(),
+        child: Row(
+          children: [
+            Text(
+              label,
+              style: const TextStyle(fontSize: 13),
+            ),
+            const SizedBox(width: 8),
+            Expanded(
+              child: DropdownButton<T?>(
+                value: value,
+                underline: Container(),
+                onChanged: onChanged,
+                isExpanded: true,
+                items: items.keys
+                    .map((key) => DropdownMenuItem<T>(
+                          value: key,
+                          child: Text(
+                            items[key]!,
+                            style: TextStyle(fontSize: 13, color: Theme.of(context).primaryColor),
+                          ),
+                        ))
+                    .toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
