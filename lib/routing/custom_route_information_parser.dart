de9a73fb80f858af593abce970922fa0103df9e4
kfranic1 <karlofranic0@gmail.com>
2023-08-17 10:35:24 +0200

    Update flutter dependencies to match `v3.13.0`

--- lib/routing/custom_route_information_parser.dart
+++ lib/routing/custom_route_information_parser.dart
@@ -4,15 +4,15 @@ import 'package:nightlife/routing/configuraiton.dart';
 class CustomRouteInformationParser extends RouteInformationParser<Configuration> {
   @override
   Future<Configuration> parseRouteInformation(RouteInformation routeInformation) async {
-    String location = routeInformation.location!;
+    String location = routeInformation.uri.path;
     if (Uri.parse(location).pathSegments.isEmpty) return Configuration.home();
     return Configuration.otherPage(Uri.decodeFull(location).substring(1));
   }
 
   @override
   RouteInformation restoreRouteInformation(Configuration configuration) {
-    if (configuration.isHomePage) return const RouteInformation(location: '/');
-    if (configuration.isOtherPage) return RouteInformation(location: '/${configuration.pathName}');
-    return const RouteInformation(location: '/');
+    if (configuration.isHomePage) return RouteInformation(uri: Uri.parse('/'));
+    if (configuration.isOtherPage) return RouteInformation(uri: Uri.parse('/${configuration.pathName}'));
+    return RouteInformation(uri: Uri.parse('/'));
   }
 }
