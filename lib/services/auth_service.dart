cb81e6c11b62fbefd0c967456b480091a9160d3f
kfranic1 <karlofranic0@gmail.com>
2023-09-21 23:42:49 +0200

    Add `GoogleAuthProvider`

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -5,8 +5,20 @@ class AuthService {
 
   Stream<User?> get authStateChanges => _auth.authStateChanges();
 
+  bool get hasUser => _auth.currentUser != null;
+
   Future<void> signIn({required String email, required String password}) async {
     await _auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<void> signInWithGoogle() async => await _signInWithProvider(GoogleAuthProvider());
+
+  Future<void> _signInWithProvider(var provider) async {
+    try {
+      await _auth.signInWithPopup(provider);
+    } catch (e) {
+      print(e);
+    }
   }
 
   Future<String?> signOut() async {
