821621acf705483ca7a6c507ba04cd4815053281
kfranic1 <karlofranic0@gmail.com>
2023-09-25 20:19:47 +0200

    Update `AppBar`
    Add Profile and Instagram shortcut to appbar

--- lib/pages/profile_page/profile_page.dart
+++ lib/pages/profile_page/profile_page.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flag/flag_enum.dart';
 import 'package:flutter/material.dart';
 import 'package:nightlife/pages/profile_page/auh_page.dart';
 import 'package:nightlife/services/auth_service.dart';
+import 'package:nightlife/widgets/language_icon_button.dart';
 import 'package:provider/provider.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -22,7 +24,14 @@ class ProfilePage extends StatelessWidget {
                 Icons.logout,
                 color: Colors.black,
               ),
-            )
+            ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              LanguageIconButton(flagsCode: FlagsCode.HR, locale: Locale('hr')),
+              LanguageIconButton(flagsCode: FlagsCode.GB, locale: Locale('en')),
+            ],
+          ),
         ],
       ),
     );
