29df054a548b1816a31083e998732367a7bdc78e
kfranic1 <karlofranic0@gmail.com>
2023-10-04 00:16:23 +0200

    Create custom `AppBar` for each club

--- lib/routing/custom_router_delegate.dart
+++ lib/routing/custom_router_delegate.dart
@@ -3,8 +3,6 @@ import 'package:nightlife/pages/admin_page/admin_page.dart';
 import 'package:nightlife/pages/error_page.dart';
 import 'package:nightlife/routing/configuraiton.dart';
 import 'package:nightlife/routing/routes.dart';
-import 'package:nightlife/widgets/app_bar.dart';
-import 'package:nightlife/widgets/options_drawer.dart';
 import 'package:provider/provider.dart';
 
 import '../helpers/club_list.dart';
@@ -25,49 +23,34 @@ class CustomRouterDelegate extends RouterDelegate<Configuration> with ChangeNoti
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(kToolbarHeight),
-        child: Builder(builder: (context) {
-          return appBar(
-            onPressedHome: goToHome,
-            onPressedDrawer: () => Scaffold.of(context).openEndDrawer(),
-          );
-        }),
-      ),
-      endDrawer: const Drawer(
-        width: 250,
-        child: OptionsDrawer(),
-      ),
-      body: Navigator(
-        key: navigatorKey,
-        pages: [
-          if (_configuration.isHomePage)
-            const MaterialPage(child: HomePage())
-          else if (_configuration.isAdmin)
-            const MaterialPage(child: AdminPage())
-          else if (_configuration.isClub)
-            MaterialPage(child: Builder(
-              builder: (context) {
-                Club? club = context.read<ClubList>().findClubByName(_configuration.info!);
-                if (club == null) return const ErrorPage();
-                return Provider.value(
-                  value: club,
-                  child: const ClubPage(),
-                );
-              },
-            ))
-          else
-            const MaterialPage(child: ErrorPage())
-        ],
-        onPopPage: (route, result) {
-          if (!route.didPop(result)) return false;
-          if (_configurationsStack.length > 1) _configurationsStack.removeLast();
-          _configuration = _configurationsStack.last;
-          notifyListeners();
-          return true;
-        },
-      ),
+    return Navigator(
+      key: navigatorKey,
+      pages: [
+        if (_configuration.isHomePage)
+          const MaterialPage(child: HomePage())
+        else if (_configuration.isAdmin)
+          const MaterialPage(child: AdminPage())
+        else if (_configuration.isClub)
+          MaterialPage(child: Builder(
+            builder: (context) {
+              Club? club = context.read<ClubList>().findClubByName(_configuration.info!);
+              if (club == null) return const ErrorPage();
+              return Provider.value(
+                value: club,
+                child: const ClubPage(),
+              );
+            },
+          ))
+        else
+          const MaterialPage(child: ErrorPage())
+      ],
+      onPopPage: (route, result) {
+        if (!route.didPop(result)) return false;
+        if (_configurationsStack.length > 1) _configurationsStack.removeLast();
+        _configuration = _configurationsStack.last;
+        notifyListeners();
+        return true;
+      },
     );
   }
 
