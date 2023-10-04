ccc6d02b3136b055e82354bba2cedbff41e1b4ed
kfranic1 <karlofranic0@gmail.com>
2023-10-04 14:55:21 +0200

    Refactoring

--- lib/pages/profile_page/profile_page.dart
+++ lib/pages/profile_page/profile_page.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flag/flag_enum.dart';
 import 'package:flutter/material.dart';
-import 'package:nightlife/pages/profile_page/auh_page.dart';
+import 'package:nightlife/pages/profile_page/subpages/auth_page.dart';
 import 'package:nightlife/services/auth_service.dart';
 import 'package:nightlife/widgets/language_icon_button.dart';
 import 'package:provider/provider.dart';
@@ -12,28 +12,32 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     User? user = context.watch<User?>();
-    if (user == null) return const AuthPage();
-    return Center(
-      child: Column(
-        children: [
-          Text(user.displayName ?? ''),
-          if (context.read<AuthService>().hasUser)
-            IconButton(
-              onPressed: () => context.read<AuthService>().signOut(),
-              icon: const Icon(
-                Icons.logout,
-                color: Colors.black,
-              ),
-            ),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold(
+      body: Builder(builder: (context) {
+        if (user == null) return const AuthPage();
+        return Center(
+          child: Column(
             children: [
-              LanguageIconButton(flagsCode: FlagsCode.HR, locale: Locale('hr')),
-              LanguageIconButton(flagsCode: FlagsCode.GB, locale: Locale('en')),
+              Text(user.displayName ?? ''),
+              if (context.read<AuthService>().hasUser)
+                IconButton(
+                  onPressed: () => context.read<AuthService>().signOut(),
+                  icon: const Icon(
+                    Icons.logout,
+                    color: Colors.black,
+                  ),
+                ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  LanguageIconButton(flagsCode: FlagsCode.HR, locale: Locale('hr')),
+                  LanguageIconButton(flagsCode: FlagsCode.GB, locale: Locale('en')),
+                ],
+              ),
             ],
           ),
-        ],
-      ),
+        );
+      }),
     );
   }
 }
