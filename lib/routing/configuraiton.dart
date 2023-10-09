8605bec3a410fb1a2de6d2efcab267feb52441e5
kfranic1 <karlofranic0@gmail.com>
2023-10-09 02:05:24 +0200

    Fix routing and update UI
    add login and signup routes and redirect to profile

--- lib/routing/configuraiton.dart
+++ lib/routing/configuraiton.dart
@@ -4,15 +4,25 @@ class Configuration {
   final String? path;
   final String? info;
 
-  Configuration.home() : path = null, info = null;
+  Configuration.home()
+      : path = null,
+        info = null;
 
   Configuration.other(this.path) : info = null;
 
   Configuration.club(this.info) : path = Routes.club;
 
+  Configuration.login()
+      : path = Routes.login,
+        info = null;
+  Configuration.signup()
+      : path = Routes.signup,
+        info = null;
+
   bool get isHomePage => path == null;
   bool get isAdmin => path == Routes.admin;
   bool get isProfile => path == Routes.profile;
   bool get isClub => path == Routes.club;
-
+  bool get isLogin => path == Routes.login;
+  bool get isSignup => path == Routes.signup;
 }
