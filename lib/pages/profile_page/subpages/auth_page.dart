847dd3f140305b2ee53206de888135ee0e6ea04a
kfranic1 <karlofranic0@gmail.com>
2023-10-09 02:05:24 +0200

    Fix routing and update UI
    add login and signup routes and redirect to profile

--- lib/pages/profile_page/subpages/auth_page.dart
+++ lib/pages/profile_page/subpages/auth_page.dart
@@ -1,18 +1,156 @@
 import 'package:flutter/material.dart';
-import 'package:nightlife/pages/profile_page/subpages/login_page.dart';
-import 'package:nightlife/pages/profile_page/subpages/signup_page.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:nightlife/model/person.dart';
+import 'package:nightlife/pages/profile_page/input_field.dart';
+import 'package:nightlife/pages/profile_page/profile_action_button.dart';
+import 'package:nightlife/routing/custom_router_delegate.dart';
+import 'package:nightlife/services/auth_service.dart';
+import 'package:provider/provider.dart';
 
 class AuthPage extends StatefulWidget {
-  const AuthPage({super.key});
+  const AuthPage({
+    super.key,
+    required this.isLogin,
+    required this.title,
+    required this.message,
+    required this.swapQuestion,
+    required this.swapAction,
+  });
+
+  final bool isLogin;
+  final String title;
+  final String message;
+  final String swapQuestion;
+  final String swapAction;
 
   @override
   State<AuthPage> createState() => _AuthPageState();
 }
 
 class _AuthPageState extends State<AuthPage> {
-  bool showLogin = true;
+  final TextEditingController _name = TextEditingController();
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return showLogin ? LoginPage(signUp: () => setState(() => showLogin = false)) : SignUpPage(login: () => setState(() => showLogin = true));
+    AuthService auth = context.read<AuthService>();
+    CustomRouterDelegate router = context.read<CustomRouterDelegate>();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => router.goBack(),
+          color: Colors.black,
+          splashRadius: 0.1,
+        ),
+        titleSpacing: 0,
+        title: Text(
+          widget.title,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: StreamBuilder<Person?>(
+          stream: auth.authStateChanges,
+          builder: (context, snapshot) {
+            if (snapshot.hasData && snapshot.data != null) {
+              WidgetsBinding.instance.addPostFrameCallback((_) => router.goToProfile());
+              return const SizedBox.shrink();
+            }
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 40),
+                  Text(
+                    widget.message,
+                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
+                  ),
+                  const SizedBox(height: 80),
+                  SizedBox(
+                    width: 320,
+                    child: Column(
+                      children: [
+                        if (!widget.isLogin) ...[
+                          InputField(
+                            controller: _name,
+                            labelText: 'Name',
+                            icon: const Icon(Icons.person),
+                          ),
+                          const SizedBox(height: 20),
+                        ],
+                        InputField(
+                          controller: _email,
+                          labelText: 'Email',
+                          icon: const Icon(Icons.email),
+                        ),
+                        const SizedBox(height: 20),
+                        InputField(
+                          controller: _password,
+                          labelText: 'Password',
+                          icon: const Icon(Icons.lock),
+                          obscureText: true,
+                        ),
+                        if (widget.isLogin)
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: TextButton(
+                              onPressed: () {},
+                              child: const Text("Forgot your password?"),
+                            ),
+                          )
+                      ],
+                    ),
+                  ),
+                  const Expanded(child: SizedBox()),
+                  Column(
+                    children: [
+                      ProfileActionButton(
+                        width: 310,
+                        action: () => widget.isLogin
+                            ? auth.signIn(email: _email.text, password: _password.text)
+                            : auth.signUp(email: _email.text, password: _password.text, name: _name.text),
+                        label: Text(
+                          widget.title.toUpperCase(),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      ProfileActionButton(
+                        width: 310,
+                        action: () => auth.signInWithGoogle(),
+                        backgroundColor: Colors.white,
+                        icon: const Icon(
+                          FontAwesomeIcons.google,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 18),
+                  TextButton(
+                    onPressed: () => widget.isLogin ? router.goToSignup() : router.goToLogin(),
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(text: widget.swapQuestion),
+                          TextSpan(
+                            text: widget.swapAction,
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          )
+                        ],
+                        style: const TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                ],
+              ),
+            );
+          }),
+    );
   }
 }
--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -3,8 +3,12 @@ abstract class Routes {
   static const String club = "club";
   static const String admin = "admin";
   static const String profile = "profile";
+  static const String login = "login";
+  static const String signup = "signup";
 
   void goToHome() {}
   void goToClub(String clubName) {}
   void goToProfile() {}
+  void goToLogin(){}
+  void goToSignup(){}
 }
