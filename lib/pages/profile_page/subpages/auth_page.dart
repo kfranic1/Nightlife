fbf27952f832da2a96c6e4a1687ac2c407ee3061
kfranic1 <karlofranic0@gmail.com>
2023-10-11 19:59:25 +0200

    Fix `goBack` routing

--- lib/pages/profile_page/subpages/auth_page.dart
+++ lib/pages/profile_page/subpages/auth_page.dart
@@ -57,7 +57,7 @@ class _AuthPageState extends State<AuthPage> {
           stream: auth.authStateChanges,
           builder: (context, snapshot) {
             if (snapshot.hasData && snapshot.data != null) {
-              WidgetsBinding.instance.addPostFrameCallback((_) => router.goToProfile());
+              WidgetsBinding.instance.addPostFrameCallback((_) => router.handleLogin());
               return const SizedBox.shrink();
             }
             return Center(
