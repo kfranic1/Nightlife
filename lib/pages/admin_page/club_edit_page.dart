26a7f4598c136f77b78ce6c53dd16807cb719f70
kfranic1 <karlofranic0@gmail.com>
2023-10-17 14:52:41 +0200

    Update 'AdminPage' access

--- lib/pages/admin_page/club_edit_page.dart
+++ lib/pages/admin_page/club_edit_page.dart
@@ -29,155 +29,160 @@ class _ClubEditPageState extends State<ClubEditPage> {
 
   @override
   Widget build(BuildContext context) {
-    _club = context.watch<Club>();
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          const Text("Club info"),
-          ClubTextField(
-            labelText: "Club name",
-            initialValue: _club.name,
-            onChanged: (value) => _club.name = value,
-            validate: true,
-          ),
-          ClubTextField(
-            labelText: "Description Croatian",
-            initialValue: _club.descriptionHr,
-            onChanged: (value) => _club.descriptionHr = value,
-            maxLines: null,
-          ),
-          ClubTextField(
-            labelText: "Description English",
-            initialValue: _club.descriptionEn,
-            onChanged: (value) => _club.descriptionEn = value,
-            maxLines: null,
-          ),
-          ClubTextField(
-            labelText: 'Address',
-            initialValue: _club.location,
-            onChanged: (value) => _club.location = value,
-            validate: true,
-            icon: const Icon(Icons.location_pin),
-          ),
-          ClubTextField(
-            labelText: 'Phone',
-            initialValue: _club.contacts[Contact.phone] ?? '',
-            onChanged: (value) => _club.contacts[Contact.phone] = value,
-            icon: Icon(Contact.phone.icon),
-          ),
-          ClubTextField(
-            labelText: 'Web',
-            initialValue: _club.contacts[Contact.web] ?? '',
-            onChanged: (value) => _club.contacts[Contact.web] = value,
-            icon: Icon(Contact.web.icon),
-          ),
-          ClubTextField(
-            labelText: 'Email',
-            initialValue: _club.contacts[Contact.email] ?? '',
-            onChanged: (value) => _club.contacts[Contact.email] = value,
-            icon: Icon(Contact.email.icon),
-          ),
-          ClubTextField(
-            labelText: 'Instagram',
-            initialValue: _club.socialMedia[SocialMedia.instagram] ?? '',
-            onChanged: (value) => _club.socialMedia[SocialMedia.instagram] = value,
-            icon: Icon(SocialMedia.instagram.icon),
-          ),
-          ClubTextField(
-            labelText: 'Facebook',
-            initialValue: _club.socialMedia[SocialMedia.facebook] ?? '',
-            onChanged: (value) => _club.socialMedia[SocialMedia.facebook] = value,
-            icon: Icon(SocialMedia.facebook.icon),
-          ),
-          ClubTextField(
-            labelText: 'Image url',
-            initialValue: _club.imageUrl,
-            onChanged: (value) => _club.imageUrl = value,
-            icon: const Icon(Icons.image),
-            maxLines: null,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: DecoratedContainer(
+    _club = context.read<Club>();
+    return SingleChildScrollView(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            ClubTextField(
+              labelText: "Club name",
+              initialValue: _club.name,
+              onChanged: (value) => _club.name = value,
+              validate: true,
+            ),
+            ClubTextField(
+              labelText: "Description Croatian",
+              initialValue: _club.descriptionHr,
+              onChanged: (value) => _club.descriptionHr = value,
+              maxLines: null,
+            ),
+            ClubTextField(
+              labelText: "Description English",
+              initialValue: _club.descriptionEn,
+              onChanged: (value) => _club.descriptionEn = value,
+              maxLines: null,
+            ),
+            ClubTextField(
+              labelText: 'Address',
+              initialValue: _club.location,
+              onChanged: (value) => _club.location = value,
+              validate: true,
+              icon: const Icon(Icons.location_pin),
+            ),
+            ClubTextField(
+              labelText: 'Phone',
+              initialValue: _club.contacts[Contact.phone] ?? '',
+              onChanged: (value) => _club.contacts[Contact.phone] = value,
+              icon: Icon(Contact.phone.icon),
+            ),
+            ClubTextField(
+              labelText: 'Web',
+              initialValue: _club.contacts[Contact.web] ?? '',
+              onChanged: (value) => _club.contacts[Contact.web] = value,
+              icon: Icon(Contact.web.icon),
+            ),
+            ClubTextField(
+              labelText: 'Email',
+              initialValue: _club.contacts[Contact.email] ?? '',
+              onChanged: (value) => _club.contacts[Contact.email] = value,
+              icon: Icon(Contact.email.icon),
+            ),
+            ClubTextField(
+              labelText: 'Instagram',
+              initialValue: _club.socialMedia[SocialMedia.instagram] ?? '',
+              onChanged: (value) => _club.socialMedia[SocialMedia.instagram] = value,
+              icon: Icon(SocialMedia.instagram.icon),
+            ),
+            ClubTextField(
+              labelText: 'Facebook',
+              initialValue: _club.socialMedia[SocialMedia.facebook] ?? '',
+              onChanged: (value) => _club.socialMedia[SocialMedia.facebook] = value,
+              icon: Icon(SocialMedia.facebook.icon),
+            ),
+            ClubTextField(
+              labelText: 'Image url',
+              initialValue: _club.imageUrl,
+              onChanged: (value) => _club.imageUrl = value,
+              icon: const Icon(Icons.image),
+              maxLines: null,
+            ),
+            Padding(
               padding: const EdgeInsets.all(8.0),
-              child: ExpansionTile(
-                maintainState: true,
-                title: const Text("Work days"),
-                children: _club.workHours.keys.toList().rearrange((p0, p1) => p0.index - p1.index).map((dayOfWeek) {
-                  WorkDay day = _club.workHours[dayOfWeek]!;
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: DecoratedContainer(
-                      child: Row(children: [
-                        SizedBox(width: 80, child: Text(dayOfWeek.name)),
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 8),
-                          width: 200,
-                          child: ClubTextField(
-                            initialValue: day.hours,
-                            labelText: "hh:mm - hh:mm",
-                            onChanged: (value) => day.hours = value,
-                            validate: true,
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                if (day.typeOfMusic.isNotEmpty) return "Missing working hours";
-                                return null;
-                              }
-                              if (!timePattern.hasMatch(value)) return "The time is not in correct format";
-                              if (day.typeOfMusic.isEmpty) return "Select at least one genre";
-                              if (day.typeOfMusic.length > 3) return "Select at most 3 genres";
-                              return null;
-                            },
-                          ),
+              child: DecoratedContainer(
+                padding: const EdgeInsets.all(8.0),
+                child: ExpansionTile(
+                  maintainState: true,
+                  title: const Text("Work days"),
+                  children: _club.workHours.keys.toList().rearrange((p0, p1) => p0.index - p1.index).map((dayOfWeek) {
+                    WorkDay day = _club.workHours[dayOfWeek]!;
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: DecoratedContainer(
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                SizedBox(width: 80, child: Text(dayOfWeek.name)),
+                                Container(
+                                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                                  width: 200,
+                                  child: ClubTextField(
+                                    initialValue: day.hours,
+                                    labelText: "hh:mm - hh:mm",
+                                    onChanged: (value) => day.hours = value,
+                                    validate: true,
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        if (day.typeOfMusic.isNotEmpty) return "Missing working hours";
+                                        return null;
+                                      }
+                                      if (!timePattern.hasMatch(value)) return "The time is not in correct format";
+                                      if (day.typeOfMusic.isEmpty) return "Select at least one genre";
+                                      if (day.typeOfMusic.length > 3) return "Select at most 3 genres";
+                                      return null;
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                            ExpansionTile(
+                              title: Text("Types of music (${day.typeOfMusic.join(', ')})"),
+                              children: TypeOfMusic.values.toList().rearrange((p0, p1) => p0.name.compareTo(p1.name)).map((type) {
+                                return CheckboxListTile(
+                                  controlAffinity: ListTileControlAffinity.leading,
+                                  title: Text(type.toString()),
+                                  value: day.typeOfMusic.contains(type),
+                                  onChanged: (bool? value) {
+                                    setState(() {
+                                      if (value!) {
+                                        day.typeOfMusic.add(type);
+                                      } else {
+                                        day.typeOfMusic.remove(type);
+                                      }
+                                    });
+                                  },
+                                );
+                              }).toList(),
+                            ),
+                          ],
                         ),
-                        Expanded(
-                          child: ExpansionTile(
-                            title: Text("Types of music (${day.typeOfMusic.join(', ')})"),
-                            children: TypeOfMusic.values.toList().rearrange((p0, p1) => p0.name.compareTo(p1.name)).map((type) {
-                              return CheckboxListTile(
-                                controlAffinity: ListTileControlAffinity.leading,
-                                title: Text(type.toString()),
-                                value: day.typeOfMusic.contains(type),
-                                onChanged: (bool? value) {
-                                  setState(() {
-                                    if (value!) {
-                                      day.typeOfMusic.add(type);
-                                    } else {
-                                      day.typeOfMusic.remove(type);
-                                    }
-                                  });
-                                },
-                              );
-                            }).toList(),
-                          ),
-                        ),
-                      ]),
-                    ),
-                  );
-                }).toList(),
+                      ),
+                    );
+                  }).toList(),
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FormButton(
-              action: () async {
-                if (_formKey.currentState!.validate()) {
-                  _formKey.currentState!.save();
-                  await FormButton.tryAction(context, () async {
-                    if (_club.id.isEmpty)
-                      await Club.createClub(_club);
-                    else
-                      await Club.updateClub(_club);
-                  });
-                }
-              },
-              label: "Save",
-              color: Colors.green,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FormButton(
+                action: () async {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    await FormButton.tryAction(context, () async {
+                      if (_club.id.isEmpty)
+                        await Club.createClub(_club);
+                      else
+                        await Club.updateClub(_club);
+                    });
+                  }
+                },
+                label: "Save",
+                color: Colors.green,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/routing/custom_route_information_parser.dart
+++ lib/routing/custom_route_information_parser.dart
@@ -7,11 +7,14 @@ class CustomRouteInformationParser extends RouteInformationParser<Configuration>
   Future<Configuration> parseRouteInformation(RouteInformation routeInformation) async {
     final Uri uri = routeInformation.uri;
     if (uri.pathSegments.isEmpty) return Configuration.home();
-    if (uri.pathSegments[0] == Routes.admin) return Configuration.other(Routes.admin);
+    if (uri.pathSegments[0] == Routes.admin && uri.pathSegments.length > 1) {
+      String decodedInfo = Uri.decodeQueryComponent(uri.pathSegments[1]);
+      return Configuration.admin(decodedInfo);
+    }
     if (uri.pathSegments[0] == Routes.profile) {
-      if (uri.pathSegments.length == 1) return Configuration.other(Routes.profile);
-      if (uri.pathSegments[1] == Routes.login) return Configuration.other(Routes.login);
-      if (uri.pathSegments[1] == Routes.signup) return Configuration.other(Routes.signup);
+      if (uri.pathSegments.length == 1) return Configuration.profile();
+      if (uri.pathSegments[1] == Routes.login) return Configuration.login();
+      if (uri.pathSegments[1] == Routes.signup) return Configuration.signup();
     }
     if (uri.pathSegments[0] == Routes.club && uri.pathSegments.length > 1) {
       String decodedInfo = Uri.decodeQueryComponent(uri.pathSegments[1]);
@@ -23,10 +26,13 @@ class CustomRouteInformationParser extends RouteInformationParser<Configuration>
   @override
   RouteInformation restoreRouteInformation(Configuration configuration) {
     if (configuration.isHomePage) return RouteInformation(uri: Uri.parse('/'));
-    if (configuration.isAdmin) return RouteInformation(uri: Uri.parse("/${Routes.admin}"));
     if (configuration.isProfile) return RouteInformation(uri: Uri.parse("/${Routes.profile}"));
     if (configuration.isLogin) return RouteInformation(uri: Uri.parse("/${Routes.profile}/${Routes.login}"));
     if (configuration.isSignup) return RouteInformation(uri: Uri.parse("/${Routes.profile}/${Routes.signup}"));
+    if (configuration.isAdmin) {
+      String encodedInfo = Uri.encodeQueryComponent(configuration.info!);
+      return RouteInformation(uri: Uri(path: "/${Routes.admin}/$encodedInfo"));
+    }
     if (configuration.isClub) {
       String encodedInfo = Uri.encodeQueryComponent(configuration.info!);
       return RouteInformation(uri: Uri(path: "/${Routes.club}/$encodedInfo"));
