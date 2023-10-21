ca488f6e70ddf5ad2d6a72c5e1d43fa30418a52f
kfranic1 <karlofranic0@gmail.com>
2023-10-21 13:08:04 +0200

    Refactoring
    Extract collections to separate file
    Moved some methods around
    Added transactions where needed

--- lib/extensions/person_extension.dart
+++ lib/extensions/person_extension.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:nightlife/helpers/collections_list.dart';
 import 'package:nightlife/model/club.dart';
 import 'package:nightlife/model/person.dart';
 
@@ -6,16 +7,28 @@ extension PersonExtension on Person {
   Future<void> handleFavourite(Club club) async {
     final bool addingToFavourites = !favourites.contains(club.id);
 
-    await FirebaseFirestore.instance.runTransaction((transaction) async {
+    try {
+      await FirebaseFirestore.instance.runTransaction((transaction) async {
+        if (addingToFavourites) {
+          transaction.update(CollectionList.userCollection.doc(id), {
+            "favourites": FieldValue.arrayUnion([club.id])
+          });
+          transaction.update(CollectionList.clubCollection.doc(id), {'favoriteCount': FieldValue.increment(1)});
+        } else {
+          transaction.update(CollectionList.userCollection.doc(id), {
+            "favourites": FieldValue.arrayRemove([club.id])
+          });
+          transaction.update(CollectionList.clubCollection.doc(id), {'favoriteCount': FieldValue.increment(-1)});
+        }
+      });
+
       if (addingToFavourites) {
         favourites.add(club.id);
-        transaction.update(personReference, {"favourites": favourites});
-        transaction.update(club.clubReference, {'favoriteCount': FieldValue.increment(1)});
       } else {
         favourites.remove(club.id);
-        transaction.update(personReference, {"favourites": favourites});
-        transaction.update(club.clubReference, {'favoriteCount': FieldValue.increment(-1)});
       }
-    });
+    } catch (e) {
+      print("Transaction failed: $e");
+    }
   }
 }
