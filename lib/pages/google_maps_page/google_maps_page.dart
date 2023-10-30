b7e6a93b44327c86907ca3aa796d649fd46b7cfb
kfranic1 <karlofranic0@gmail.com>
2023-10-30 13:02:30 +0100

    Map is now on `HomeScreen` as second tab
    Map is adjusted via filters
    Removed routing for map

--- lib/pages/google_maps_page/google_maps_page.dart
+++ lib/pages/google_maps_page/google_maps_page.dart
@@ -7,9 +7,7 @@ import 'package:nightlife/extensions/geopoint_extension.dart';
 import 'package:nightlife/helpers/club_list.dart';
 import 'package:nightlife/model/club.dart';
 import 'package:nightlife/pages/google_maps_page/marker/custom_marker.dart';
-import 'package:nightlife/routing/custom_router_delegate.dart';
 import 'package:provider/provider.dart';
-import 'package:simple_gradient_text/simple_gradient_text.dart';
 
 class GoogleMapsPage extends StatefulWidget {
   const GoogleMapsPage({super.key});
@@ -18,96 +16,65 @@ class GoogleMapsPage extends StatefulWidget {
   State<GoogleMapsPage> createState() => _GoogleMapsPageState();
 }
 
-class _GoogleMapsPageState extends State<GoogleMapsPage> {
+class _GoogleMapsPageState extends State<GoogleMapsPage> with AutomaticKeepAliveClientMixin {
   final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
-  List<Point> clubPoints = []; // To store calculated screen positions of clubs
+  late Map<Club, ScreenCoordinate> clubPoints;
   double markerRadius = 25;
 
-  // Method to update the screen positions of markers based on map camera position
   Future<void> _updateMarkers(List<Club> clubs) async {
     GoogleMapController controller = await _controller.future;
     clubPoints.clear();
-
-    for (var club in clubs) {
-      final screenPoint = await controller.getScreenCoordinate(club.location.pin.latLng);
-      clubPoints.add(Point(
-        x: screenPoint.x.toDouble(),
-        y: screenPoint.y.toDouble(),
-      ));
-    }
-
+    await Future.forEach(clubs, (club) async => clubPoints[club] = (await controller.getScreenCoordinate(club.location.pin.latLng)));
     setState(() {});
   }
 
   @override
+  void initState() {
+    clubPoints = Map.fromIterable(context.read<ClubList>().clubs, key: (element) => element, value: (element) => const ScreenCoordinate(x: 0, y: 0));
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    List<Club> clubs = context.read<ClubList>().clubs;
-    double markerRadius = 25;
-    double avgLat = clubs.map((club) => club.location.pin.latLng.latitude).reduce((a, b) => a + b) / clubs.length;
-    double avgLng = clubs.map((club) => club.location.pin.latLng.longitude).reduce((a, b) => a + b) / clubs.length;
-    LatLng avgLatLng = LatLng(avgLat, avgLng);
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        leadingWidth: 0,
-        automaticallyImplyLeading: false,
-        title: TextButton(
-          style: TextButton.styleFrom(padding: EdgeInsets.zero),
-          onPressed: () => context.read<CustomRouterDelegate>().goToHome(),
-          child: GradientText(
-            'NIGHTLIFE ZAGREB',
-            style: Theme.of(context).textTheme.headlineLarge,
-            colors: const [
-              Color.fromARGB(255, 0, 255, 255),
-              Color.fromARGB(255, 255, 0, 255),
-            ],
+    super.build(context);
+    List<Club> clubs = context.watch<ClubList>().filteredClubs;
+    return Stack(
+      children: [
+        GoogleMap(
+          padding: const EdgeInsets.only(bottom: 100.0),
+          mapType: MapType.normal,
+          initialCameraPosition: CameraPosition(
+            target: context.read<ClubList>().clubCenter,
+            zoom: 14,
           ),
+          onMapCreated: (GoogleMapController controller) async {
+            _controller.complete(controller);
+            await controller.setMapStyle(await rootBundle.loadString('assets/maps/map_style.txt'));
+            await _updateMarkers(clubs);
+          },
+          onCameraMove: (_) async => await _updateMarkers(clubs),
+          onCameraIdle: () async => await _updateMarkers(clubs),
+          zoomControlsEnabled: true,
+          scrollGesturesEnabled: true,
+          rotateGesturesEnabled: false,
+          tiltGesturesEnabled: false,
+          myLocationEnabled: true,
+          myLocationButtonEnabled: true,
+          mapToolbarEnabled: false,
         ),
-      ),
-      body: Center(
-        child: Stack(
-          children: [
-            GoogleMap(
-              padding: const EdgeInsets.only(bottom: 100.0),
-              mapType: MapType.normal,
-              initialCameraPosition: CameraPosition(
-                target: avgLatLng,
-                zoom: 14,
-              ),
-              onMapCreated: (GoogleMapController controller) async {
-                _controller.complete(controller);
-                await controller.setMapStyle(await rootBundle.loadString('assets/maps/map_style.txt'));
-                await _updateMarkers(clubs);
-              },
-              onCameraMove: (_) async => await _updateMarkers(clubs),
-              onCameraIdle: () async => await _updateMarkers(clubs),
-              zoomControlsEnabled: true,
-              scrollGesturesEnabled: true,
-              rotateGesturesEnabled: false,
-              tiltGesturesEnabled: false,
-              myLocationEnabled: true,
-              myLocationButtonEnabled: true,
-              mapToolbarEnabled: false,
+        for (Club club in clubs)
+          Positioned(
+            left: clubPoints[club]!.x - markerRadius,
+            top: clubPoints[club]!.y - 2.8 * markerRadius,
+            child: CustomMarker(
+              imageUrl: club.imageUrl,
+              radius: markerRadius,
             ),
-            for (var i = 0; i < clubPoints.length; i++)
-              Positioned(
-                left: clubPoints[i].x - markerRadius,
-                top: clubPoints[i].y - 2.8 * markerRadius,
-                child: CustomMarker(
-                  imageUrl: clubs[i].imageUrl,
-                  radius: markerRadius,
-                ),
-              ),
-          ],
-        ),
-      ),
+          ),
+      ],
     );
   }
-}
 
-class Point {
-  final double x;
-  final double y;
-
-  Point({required this.x, required this.y});
+  @override
+  bool get wantKeepAlive => true;
 }
