025b91ec5f6012b8bc1331c26476e45bd7742fc3
kfranic1 <karlofranic0@gmail.com>
2023-10-31 11:20:10 +0100

    Update filters
    Removed sorting
    Added open tonight filter
    Improved UI

--- lib/helpers/club_list.dart
+++ lib/helpers/club_list.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/foundation.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:nightlife/enums/day_of_week.dart';
 import 'package:nightlife/enums/type_of_music.dart';
 import 'package:nightlife/extensions/geopoint_extension.dart';
-import 'package:nightlife/helpers/filters.dart';
 import 'package:nightlife/model/club.dart';
 
 class ClubList extends ChangeNotifier {
   List<Club> _clubs = [];
   List<Club> _filteredClubs = [];
+  bool _showOpenTonightOnly = false;
 
   String _filterText = "";
   TypeOfMusic? _typeOfMusic;
-  BaseFilter _filter = BaseFilter.filters.first;
 
   Future setup() async {
     await Club.getClubs().then((value) {
@@ -22,14 +22,18 @@ class ClubList extends ChangeNotifier {
     });
   }
 
+  bool get isFiltered => _filterText.isNotEmpty || _typeOfMusic != null || _showOpenTonightOnly;
+
   String get filterText => _filterText;
   TypeOfMusic? get typeOfMusic => _typeOfMusic;
-  BaseFilter get filter => _filter;
+  bool get showOpenTonightOnly => _showOpenTonightOnly;
   List<Club> get clubs => _clubs;
   List<Club> get filteredClubs => _filteredClubs;
 
-  void updateFilter(BaseFilter filter) {
-    _filter = filter;
+  void clearFiler() {
+    _filterText = '';
+    _typeOfMusic = null;
+    _showOpenTonightOnly = false;
     _applyFilter();
   }
 
@@ -43,10 +47,22 @@ class ClubList extends ChangeNotifier {
     _applyFilter();
   }
 
+  void updateShowOpenTonightOnly(bool value) {
+    _showOpenTonightOnly = value;
+    _applyFilter();
+  }
+
   void _applyFilter() {
     _filteredClubs = _clubs.where((club) => club.name.toLowerCase().contains(_filterText.toLowerCase())).toList();
+
     if (_typeOfMusic != null) _filteredClubs = _filteredClubs.where((element) => element.typeOfMusic.contains(_typeOfMusic)).toList();
-    _filter.applyFilter(_filteredClubs);
+
+    if (_showOpenTonightOnly) {
+      DateTime now = DateTime.now();
+      DayOfWeek today = DayOfWeek.values[now.weekday - 1];
+      _filteredClubs = _filteredClubs.where((element) => element.workHours[today]!.open).toList();
+    }
+
     notifyListeners();
   }
 
